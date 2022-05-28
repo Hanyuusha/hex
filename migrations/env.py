a21c2c36598a63aa5ababb57c3ac8bf5d2e953f6
ZeroTwo <hau.au.999@gmail.com>
2022-05-29 00:47:52 +0500

    Fix migrations

--- migrations/env.py
+++ migrations/env.py
@@ -3,7 +3,7 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import create_engine
 
-from app.config import get_sync_db_url
+from app.config.store import get_sync_db_url
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
